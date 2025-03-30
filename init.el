e4e585d5e2b0ba64babf113707299f7382a5fd28
gluesniffmonkey <harper.paul.j@gmail.com>
2025-03-30 16:15:59 +0300

    solution from David Wilson

--- init.el
+++ init.el
@@ -11,3 +11,13 @@
 
 ;; Startup echo
 (message "Emacs initialized successfully.")
+
+;; Automatically tangle our Emacs.org config file when we save it
+(defun efs/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.config/emacs/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
